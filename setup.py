34fa4a0f1325cafc318d7ccedc0e6b565d39dd50
Alex Jones <atjalex@gmail.com>
2018-12-03 11:44:00 -0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ import sys
 buildOptions = dict(packages = [], excludes = [])
 #
 
-build_exe_options = {"packages": ["os","pandas","Tkinter","numpy"],"excludes": ["notebook","PyQt4.QtNetwork","PyQt4.QtScript","sqlite3","PyQt4.QtSql","IPython", "matplotlibwidget","matplotlib","socketserver"],"include_files": "PhenoPredict_Ancillary_Files/,LICENSE-BSD.txt,LICENSE-cx.txt","optimize":2}
+build_exe_options = {"packages": ["os","pandas","Tkinter","numpy"],"excludes": ["notebook","PyQt4.QtNetwork","PyQt4.QtScript","sqlite3","PyQt4.QtSql","IPython", "matplotlibwidget","matplotlib","socketserver","curses"],
+"include_files": "PhenoForecaster_Ancillary_Files/,LICENSE-BSD.txt,LICENSE-cx.txt","optimize":0 }
 
 #build_exe_options = {"excludes": ["notebook","PyQt4.QtNetwork","PyQt4.QtScript","sqlite3","PyQt4.QtSql","mpl"],"include_files": "PhenoPredict_Ancillary_Files/,LICENSE-BSD.txt,LICENSE-cx.txt","optimize":2}
 
@@ -14,11 +15,11 @@ build_exe_options = {"packages": ["os","pandas","Tkinter","numpy"],"excludes": [
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
-    Executable('PhenoPredict.py', base=base)
+    Executable('PhenoForecaster.py', base=base)
 ]
 
-setup(name='PhenoPredict',
+setup(name='PhenoForecaster',
       version = '1.0',
-      description = 'Predicts the flowering times of angiosperm taxa',
+      description = '',
       options = dict(build_exe = build_exe_options),
       executables = executables)
